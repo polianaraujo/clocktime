c82807815ac5b8fe29812ad5b98d52101f513d86
polianaraujo <polianaellena123@gmail.com>
2024-12-27 23:15:51 -0300

    clock

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+// Atualizando o relógio com base no fuso horário
 document.getElementById('timezoneSelect').addEventListener('change', function() {
     updateClock();
 });
 
+// Atualizando o fundo com base na seleção do usuário
+document.getElementById('backgroundSelect').addEventListener('change', function() {
+    const selectedBackground = document.getElementById('backgroundSelect').value;
+    if (selectedBackground) {
+        document.body.style.backgroundImage = `url('${selectedBackground}')`;
+    } else {
+        // Se a seleção for vazia, definir o fundo padrão
+        document.body.style.backgroundImage = `url('wallpapers/background1.jpg')`;
+    }
+});
+
+// Função que atualiza o relógio
 function updateClock() {
     const timezone = document.getElementById('timezoneSelect').value;
     const now = new Date();
@@ -16,5 +29,9 @@ function updateClock() {
     document.getElementById('clock').textContent = timeString;
 }
 
+// Definir fundo inicial ao carregar a página
+document.body.style.backgroundImage = `url('wallpapers/background1.jpg')`;
+
+// Atualizando o relógio a cada segundo
 setInterval(updateClock, 1000);
 updateClock();
